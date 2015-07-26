0720e2e495636bec35baee653efbeb6bbd7a531f
Peter Herndon <tpherndon@gmail.com>
2015-07-26 12:54:57 -0400

    Adding initial tornado implementations for py2 and py3

---
+++ tornado/2/main.py
@@ -0,0 +1,22 @@
+from tornado import gen
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        data = {}
+        keys = ["key%s" % i for i in xrange(1, 11)]
+        for key in keys:
+            data[key] = self.get_query_argument(key)
+            assert isinstance(data[key], (str, unicode))
+        resp = {"status_code": 200, "status_txt": "OK", "data": data}
+        self.write(resp)
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
